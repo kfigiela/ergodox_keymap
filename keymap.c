22019b81a664986ee122c1961e91dff0d1e8391c
Kamil Figiela <kamil.figiela@gmail.com>
2019-01-16 15:02:24 +0100

    Add extra % sign for easier access %%%

--- keymap.c
+++ keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,         KC_1,      KC_2,    KC_3,    KC_4,   KC_5,    KC_BTN1,
         KC_TAB,         KC_Q,      KC_W,    KC_D,    KC_P,   KC_G,    KC_ESC,
-        /* LT(SYMB, KC_ESC) */ MO(SYMB), KC_A,      KC_R,    KC_S,    KC_T,   KC_F,
-        MO(SYM2),        KC_Z,      KC_X,    KC_C,    KC_V,   KC_B,    MO(MDIA),
+        MO(SYMB),       KC_A,      KC_R,    KC_S,    KC_T,   KC_F,
+        MO(SYM2),       KC_Z,      KC_X,    KC_C,    KC_V,   KC_B,    MO(MDIA),
         KC_LCTL,        KC_LALT,   KC_LEFT, KC_RGHT, KC_SPC,
-                                                     KC_LALT,   KC_NO,
+                                                       KC_F16,  KC_F17,
                                                                 KC_LCTL,
                                               KC_LSFT, KC_LGUI, KC_LALT,
         // right hand
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_M,    KC_N,    KC_E,    KC_I,    KC_O,       MO(SYMB),
              MO(MDIA),    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_ENT,     MO(SYM2),
                                    KC_BSPC, KC_UP,   KC_DOWN, KC_RALT,    KC_RCTL,
-             KC_NO,   KC_RALT,
+             KC_F18,  KC_F19,
              KC_RCTL,
              KC_RALT, KC_RGUI, KC_RSFT
     ),
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-       KC_WH_D, KC_NO,   KC_DQUO, KC_LBRC, KC_RBRC, KC_EXLM, KC_NO,
+       KC_WH_D, KC_PERC, KC_DQUO, KC_LBRC, KC_RBRC, KC_EXLM, KC_NO,
                 KC_UNDS, KC_QUOT, KC_LPRN, KC_RPRN, KC_QUES, KC_TAB,
        KC_WH_U, KC_NO,   KC_GRV,  KC_LCBR, KC_RCBR, KC_TRNS, KC_NO,
                          KC_DELT, KC_PGUP, KC_PGDN, KC_TRNS, KC_TRNS,
@@ -174,17 +174,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 3: Media and mouse keys
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | NumLk  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * ,--------------------------------------------------.           ,---------------------------------------------------.
+ * | NumLk  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10  |   F11  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+-------+--------|
  * | PrScr  |      | Lclk | MsUp | Rclk | WhlD |      |           |      |      | ⌥ UP |⌥ LEFT|⌥RIGHT|⌥ DOWN|   F12  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Caps   | Play |MsLeft|MsDown|MsRght| WhlU |------|           |------|   +  |   UP |  LEFT| RIGHT|  DOWN|    =   |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+-------+--------|
+ * | Caps   | Play |MsLeft|MsDown|MsRght| WhlU |------|           |------|   +  |   UP |  LEFT| RIGHT|   DOWN|    =   |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+-------+--------|
  * |        | Mute | WhlL |      | WhlR |      |      |           |      |   -  | ⌘ UP |⌘ LEFT|⌘RIGHT|⌘ DOWN|        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      | Prev | Next |  -   |                                       |      |   ,  |   .  |      |      |
- *   `----------------------------------'                                       `----------------------------------'
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+-------+--------'
+ *   |      |      | Prev | Next |  -   |                                       |      |   ,  |   .  |       |      |
+ *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | ScLk | Pause|       |  -   |  -   |
  *                                 ,------|------|------|       |------+------+------.
